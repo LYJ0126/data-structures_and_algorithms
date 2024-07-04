ad2ba5ea69cae7160f7e032e8df0b6c5ca38011f
LYJ0126 <2914233363@qq.com>
2024-07-04 13:48:07 +0800

    红黑树

--- 树/平衡树/红黑树/红黑树/test.cpp
+++ 树/平衡树/红黑树/红黑树/test.cpp
@@ -1,64 +1,54 @@
-
-
-//AVL.h中有模板类，若main函数中只包含了AVL.h，而没有包含AVL.cpp，那么编译器无法根据模板类生成相应的代码，因此会报错。
-//解决方法：将AVL.h和AVL.cpp放在一起，或者在main函数中包含AVL.cpp
-#include "AVL.h"
-using namespace std;
+#include "RedBlackTree1.h"
+#include <crtdbg.h>
 
 int main()
 {
-	AVL<int> tree;
+	//检查内存泄漏
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	RBT tree;
 	int a[] = { 3, 2, 1, 4, 5, 6, 7, 10, 9, 8 };
-	cout<<"依次添加:";
+	cout << "依次添加:";
 	for (int i = 0; i < 10; i++)
 	{
 		tree.insert(a[i]);
 	}
 	cout << endl;
 	cout << "前序遍历:";
-	tree.preOrder();
+	tree.preorder();
 	cout << endl;
 	cout << "中序遍历:";
-	tree.inOrder();
+	tree.inorder();
 	cout << endl;
 	cout << "后序遍历:";
-	tree.postOrder();
+	tree.postorder();
 	cout << endl;
-	cout << "树的高度:" << tree.height() << endl;
 	cout << "最小值:" << tree.minimum() << endl;
 	cout << "最大值:" << tree.maximum() << endl;
-	cout<<"树的详细信息:"<<endl;
+	cout << "树的详细信息:" << endl;
 	tree.print();
 	cout << "删除节点8" << endl;
 	tree.remove(8);
 	cout << "中序遍历:";
-	tree.inOrder();
+	tree.inorder();
 	cout << endl;
-	cout << "树的高度:" << tree.height() << endl;
 	cout << "树的详细信息:" << endl;
 	tree.print();
 	cout << "删除节点5" << endl;
 	tree.remove(5);
 	cout << "中序遍历:";
-	tree.inOrder();
+	tree.inorder();
 	cout << endl;
-	cout << "树的高度:" << tree.height() << endl;
 	cout << "树的详细信息:" << endl;
 	tree.print();
 	cout << "插入节点8" << endl;
 	tree.insert(8);
 	cout << "中序遍历:";
-	tree.inOrder();
+	tree.inorder();
 	cout << endl;
-	cout << "树的高度:" << tree.height() << endl;
 	cout << "树的详细信息:" << endl;
 	tree.print();
 	cout << endl;
 	cout << "1的前驱:" << tree.predecessor(1) << endl;
 	cout << "4的后继:" << tree.successor(4) << endl;
-	//检查内存泄漏
-	#ifdef _DEBUG
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	#endif
 	return 0;
 }
